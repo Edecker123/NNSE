b5b17bbb0d555f466c919094db10dbfb185c3731
ethan <ethan@e1-052936.ad.psu.edu>
2022-12-28 13:41:54 -0500

    updating tree to match my work below

--- main.py
+++ main.py
@@ -21,22 +21,8 @@ v=vgg.vgg11_bn(False)
 g=goog.GoogLeNet()
 r=res.resnet18()
 i=inc.inception_v3()
-m=torch.load('c')
 
-# m=torch.nn.BatchNorm2d(3)
-# m=torch.nn.Sequential(torch.nn.Conv2d(3,4, (2,2)), torch.nn.BatchNorm2d(4))
-torch.save(m,'c')
-x=torch.tensor([[[[1.0,3.0,1.0],[1.0,1.0,1.0],[1.0,1.0,1.0]],[[2.0,2.0,2.0],[2.0,2.0,2.0],[2.0,2.0,2.0]],[[3.0,3.0,3.0],[3.0,3.0,3.0],[3.0,3.0,3.0]]]])
-# x=torch.ones(1,3,12,12)
-
-# print(m(x))
-x.requires_grad=True
-j=torch.autograd.functional.jacobian(m, x)
-print(j.shape)
-print(j[0][2][0][0][0][0]) #it seems that each channel has a dependancy on every input channel
-                           #
-
-x2=torch.ones(1,1,784)
+x=torch.zeros(1,3,128,128)
 
 
 # m_d=parseNet(m, x2)
@@ -45,9 +31,8 @@ x2=torch.ones(1,1,784)
 r_d=parseNet(r,x)
 # i_d=parseNet(i,x)
 
-for i in r_d:
-    if type(r_d[i].operation)==torch.nn.modules.batchnorm.BatchNorm2d:
-        print(r_d[i].prev)
+print(r_d)
+
     
 
 
